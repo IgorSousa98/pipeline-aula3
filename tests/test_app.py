0749a85ca1ae0ec55306806b722159dc71b1fe03
Igor Sousa <igor_oliveira2003@outlook.com>
2026-08-29 18:08:42 +0000

    fix: restaurando teste unitario para sucesso

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,15 @@
-#from src.app import build_status
+from src.app import build_status
 
 
-#def test_build_status():
-    #assert build_status() == "Pipeline CI/CD executado com sucesso!"
+def test_build_status():
+    assert build_status() == "Pipeline CI/CD executado com sucesso!"
 
 #Simulação do Cenário de Falha
 
-from src.app import build_status
+#from src.app import build_status
 
-def test_build_status():
+#ef test_build_status():
     # Forcando falha para validar bloqueio do pipeline
-    assert build_status() == "Texto Incorreto Que Vai Falhar"
+    #assert build_status() == "Texto Incorreto Que Vai Falhar"
 
     
