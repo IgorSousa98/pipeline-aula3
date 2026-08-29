a7bbf6c6b903670652c0a56b5c9cde1519766a8c
Igor Sousa <igor_oliveira2003@outlook.com>
2026-08-29 18:03:52 +0000

    test: simulando quebra de teste unitario no CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,15 @@
+#from src.app import build_status
+
+
+#def test_build_status():
+    #assert build_status() == "Pipeline CI/CD executado com sucesso!"
+
+#Simulação do Cenário de Falha
+cat << 'EOF' > tests/test_app.py
 from src.app import build_status
 
-
 def test_build_status():
-    assert build_status() == "Pipeline CI/CD executado com sucesso!"
+    # Forcando falha para validar bloqueio do pipeline
+    assert build_status() == "Texto Incorreto Que Vai Falhar"
+EOF
     
